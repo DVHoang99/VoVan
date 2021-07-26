7273730972002589b4d43f72bf2e3d7918d7306c
DVHoang99 <dvhoang080699@gmail.com>
2021-07-26 16:09:52 +0700

    config database

--- src/configurations.js
+++ src/configurations.js
@@ -3,13 +3,13 @@ require("dotenv").config();
 module.exports = {
   database: {
     connection: {
-      host: process.env.VOVAN_DATABASE_HOST || "127.0.0.1",
+      host: process.env.VOVAN_DATABASE_HOST || "ec2-54-83-82-187.compute-1.amazonaws.com",
       port: Number(process.env.VOVAN_DATABASE_PORT) || 5432,
-      user: process.env.VOVAN_DATABASE_USER || "postgres",
+      user: process.env.VOVAN_DATABASE_USER || "satzgdavwexyiw",
       password:
         process.env.VOVAN_DATABASE_PASSWORD ||
-        "123456",
-      database: process.env.VOVAN_DATABASE_DATABASE || "vovan",
+        "5f1aaa67a376e12665ba35e3dfe92582e8591f7a0fd8b663b063d160602fd036",
+      database: process.env.VOVAN_DATABASE_DATABASE || "de7rvpiui5f86h",
       // ssl: {
       //   rejectUnauthorized: false,
       // },
