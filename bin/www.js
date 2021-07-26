ac86ea45fbea3c869169f6066fb0b77556f50963
DVHoang99 <dvhoang080699@gmail.com>
2021-07-26 21:42:58 +0700

    fix bin/www.js

--- bin/www.js
+++ bin/www.js
@@ -1,6 +1,6 @@
 const server = require("../src");
 const app = require("http").createServer(server);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("LISTEN http://localhost:3000");
 });
