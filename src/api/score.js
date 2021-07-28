f8c7a40c934bd17190bc509bccff77ccbe7ed486
DVHoang99 <dvhoang080699@gmail.com>
2021-07-28 15:32:51 +0700

    fix databse

--- src/api/score.js
+++ src/api/score.js
@@ -1,16 +1,16 @@
 module.exports.getAll = async function getAll(req, res) {
   const a = req.params.param;
   const database = req.app.get("database");
-  // const row = await database("infomation")
-  //   .where("day", "=", a)
-  //   .select("*")
-  //   .catch(() => []);
-  // const result = [];
+  const row = await database("infomation")
+    .where("day", "=", a)
+    .select("*")
+    .catch(() => []);
+  const result = [];
 
-  // for (let i = 0; i < row.length; i++) {
-  //   const result2 = [row[i].day, row[i].minh, row[i].phuong, row[i].nhung, row[i].hien];
-  //   result.push(result2);
-  // }
+  for (let i = 0; i < row.length; i++) {
+    const result2 = [row[i].day, row[i].minh, row[i].phuong, row[i].nhung, row[i].hien];
+    result.push(result2);
+  }
 
   return res.json(row);
 };
@@ -18,18 +18,16 @@ module.exports.postItem = async function postItem(req, res, next) {
   // Retrieve database connection, database database <-> knex
   const database = req.app.get("database");
   try {
-    // const result = await database("infomation").returning("*").insert({
-    //   day: req.body.day,
-    //   minh: req.body.minh,
-    //   phuong: req.body.phuong,
-    //   nhung: req.body.nhung,
-    //   hien: req.body.hien,
-    // });
-    // return res.status(200).json({
-    //   response: "ok",
-    // });
-
-    console.log("test");
+    const result = await database("infomation").returning("*").insert({
+      day: req.body.day,
+      minh: req.body.minh,
+      phuong: req.body.phuong,
+      nhung: req.body.nhung,
+      hien: req.body.hien,
+    });
+    return res.status(200).json({
+      response: "ok",
+    });
   } catch (err) {
     next(err);
   }
